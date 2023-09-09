ae9c5b94501d67260698bc3786df009484787a5f
Prince45u <brownsrikky@gmail.com>
2023-09-09 13:34:19 +0100

    HNG Stage 1

--- index.js
+++ index.js
@@ -1,28 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const DateTime = new Date();
 
-const CurrentTime = new Date().getTime();
+function TimeFunction()  {
+    const CurrentTime = new Date().getTime();
+
+    document.getElementById("time-span-id").innerHTML= CurrentTime;
+}
+    setInterval(TimeFunction, 1);
 
 
-console.log(CurrentTime)
+    const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const CurrentDay = new Date().getDay();
+    const currentDayWord = daysOfTheWeek[CurrentDay];
 
-const formattedTime = new Date(CurrentTime).toLocaleString();
+    document.getElementById("day-span-id").innerHTML= currentDayWord;
 
-console.log(formattedTime);
-
-//var TimeElement = document.getElementsByClassName("time-div")
-
-//document.querySelector(".data-item[class='time-div']").innerHTML = CurrentTime;
- document.getElementById("time-span-id").innerHTML= CurrentTime;
   });
 
 
 
-
-  const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-  const CurrentDay = new Date().getDay();
-  
-  const currentDayWord = daysOfTheWeek[CurrentDay];
-
-  document.getElementById("day-span-id").innerHTML= currentDayWord;
